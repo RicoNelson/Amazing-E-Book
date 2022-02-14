b998b4970617eb5d0ff06a120814425d54714652
Rico Nelson <riconelson10@gmail.com>
2022-02-14 14:50:10 +0700

    last commit

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('language/{locale}', function($locale){
+Route::get('/language/{locale}', function($locale){
     app()->setLocale($locale);
     session()->put('locale', $locale);
 
